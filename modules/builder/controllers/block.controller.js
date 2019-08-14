b53bca1912f55d99c9babe34dad2fd16464f7150
alexgusto <vi-wood@i.ua>
2019-08-14 20:28:36 +0300

    build: get props from component.vue

--- modules/builder/controllers/block.controller.js
+++ modules/builder/controllers/block.controller.js
@@ -2,30 +2,41 @@
 
 // const Model = db.model('Article')
 const util = require('util')
-const Vue = require('vue')
+// const Vue = require('vue')
 const path = require('path')
 const readFile = util.promisify(require('fs').readFile)
 const writeFile = util.promisify(require('fs').writeFile)
-const { createDefaultCompiler, assemble } = require('@vue/component-compiler')
-
+// const hook = require('vue-node')
+const { parseComponent } = require('vue-sfc-parser')
+// const { createDefaultCompiler, assemble } = require('@vue/component-compiler')
+const { transform } = require("@babel/core");
 const tempDir = path.resolve(process.cwd(), 'tmp')
-const compiler = createDefaultCompiler()
+// const compiler = createDefaultCompiler()
 const filePath = `${tempDir}/sections/SectionContent.vue`
 const fileName = 'SectionContent.vue'
+// const vm = require('vm');
+
 
 readFile(filePath, 'utf-8')
   .then((source) => {
-    const input = compiler.compileToDescriptor(fileName, source)
-    input.script.code = input.script.code.replace(/^\s+/gm, '')
-    input.template.code = ''
-    input.template.source = ''
-    input.styles = []
-    console.log(input)
-    const result = assemble(compiler, fileName, input, {
-      // normalizer: '~vue-runtime-helpers/dist/normalize-component.js',
-    })
+    const res = parseComponent(source);
+    const result = res.script.content
+      // console.log()
+      transform(result, {}, (err, result)=>{
+        const f = new Function(result.code.replace('export default', 'return '))
+        console.log(f().props)
+      })
+    // const input = compiler.compileToDescriptor(fileName, source)
+    // input.script.code = input.script.code.replace(/^\s+/gm, '')
+    // input.template.code = ''
+    // input.template.source = ''
+    // input.styles = []
+    // console.log(input)
+    // const result = assemble(compiler, fileName, input, {
+    //   // normalizer: '~vue-runtime-helpers/dist/normalize-component.js',
+    // })
 
-    writeFile(`${tempDir}/test.js`, result.code)
+    // writeFile(`${tempDir}/test.js`, result.code)
   })
   .catch(e => {
     console.log(e)
