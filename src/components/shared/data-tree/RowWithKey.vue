130d4fef059343137bd9fe191aef013fc8c07947
alexgusto <vi-wood@i.ua>
2019-09-10 21:14:41 +0300

    build: add toggle features for data tree

--- src/components/shared/data-tree/RowWithKey.vue
+++ src/components/shared/data-tree/RowWithKey.vue
@@ -1,36 +1,46 @@
 <template>
-  <div>
-    <button class="data-tree-toggle">+</button>
+  <div :class="isHidden && 'data-tree-closed'">
+    <button class="data-tree-toggle" @click="isHidden = !isHidden">+</button>
     <span>{{ rowName }}</span>
 
     <span>
-      <button class="data-tree-add">+</button>
-      <button class="data-tree-delete">x</button>
+      <button class="data-tree-add" @click="$emit('add')">+</button>
+      <button class="data-tree-delete" @click="$emit('delete')">x</button>
     </span>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'RowWithKey',
+export default {
+  name: "RowWithKey",
 
-    props: ['rowName']
+  props: ["rowName"],
+  data() {
+    return {
+      isHidden: true
+    };
   }
+};
 </script>
 
 <style lang="scss">
-  .data-tree {
-    &-toggle {
-      background: black;
-      padding: 3px;
-      color: #fff;
-      display: inline-block;
-      margin-right: 5px;
-      border-radius: 2px;
-    }
-
-    &-add, &-delete{
-      padding: 3px;
-    }
+.data-tree {
+  &-toggle {
+    background: black;
+    padding: 3px;
+    color: #fff;
+    display: inline-block;
+    margin-right: 5px;
+    border-radius: 2px;
   }
+
+  &-add,
+  &-delete {
+    padding: 3px;
+  }
+
+  &-closed + &-list {
+    display: none;
+  }
+}
 </style>
--- src/components/shared/data-tree/index.vue
+++ src/components/shared/data-tree/index.vue
@@ -1,116 +1,139 @@
 <script>
-  import isObject from 'lodash/isObject'
-  import RowWithKey from './RowWithKey'
-  import RowWithKeyAndValue from './RowWithKeyAndValue'
-  import set from 'lodash/set'
-  import isInteger from 'lodash/isInteger'
-  import Vue from 'vue'
+import isObject from "lodash/isObject";
+import RowWithKey from "./RowWithKey";
+import RowWithKeyAndValue from "./RowWithKeyAndValue";
+import set from "lodash/set";
+import isInteger from "lodash/isInteger";
+import Vue from "vue";
 
-  export default {
-    components: {
-      RowWithKeyAndValue,
-      RowWithKey
-    },
-    props: {
-      data: {
-        type: Object,
-        default: () => ({
-          title: 'Lorem',
-          content: 'Content',
-          image: {
-            alt: 'Image',
-            title: 'Photo'
-          },
-          block: {
-            title: 'Good',
-            list: [
-              1, 2, 3, 4
-            ],
-            col: [
-              {
-                link: '/test'
-              }
-            ]
-          }
-        })
-      }
-    },
-
-    data() {
-      return {
-        dataTree: this.data
-      }
-    },
-
-    render(h) {
-      const vm = this
-      return create(this.dataTree)
-
-      function create(data, path = '') {
-        const list = Object.entries(data)
-          .reduce((acc, [key, value]) => {
-            if (isObject(value)) {
-              acc.push(h(
-                'li',
-                [
-                  h('RowWithKey', { props: { rowName: key } }),
-
-                  // invoke function recursively
-                  create(value, createPath(path, key))
-                ]
-              ))
-            } else {
-              acc.push(h('RowWithKeyAndValue', {
-                props: {
-                  rowName: key,
-                  rowValue: value
-                },
-                on: {
-                  input(value) {
-                    setData.call(vm.dataTree, createPath(path, key), value)
-                  }
-                }
-              }))
+export default {
+  components: {
+    RowWithKeyAndValue,
+    RowWithKey
+  },
+  props: {
+    data: {
+      type: Object,
+      default: () => ({
+        title: "Lorem",
+        content: "Content",
+        image: {
+          alt: "Image",
+          title: "Photo"
+        },
+        block: {
+          title: "Good",
+          list: [1, 2, 3, 4],
+          col: [
+            {
+              link: "/test"
             }
+          ]
+        }
+      })
+    }
+  },
 
-            return acc
-          }, [])
+  data() {
+    return {
+      dataTree: this.data
+    };
+  },
 
-        return h('ul', {
-          staticClass: 'data-tree-list'
-        }, list)
-      }
+  render(h) {
+    const vm = this;
+    return create(this.dataTree);
+
+    function create(data, path = "") {
+      const list = Object.entries(data).reduce((acc, [key, value]) => {
+        if (isObject(value)) {
+          acc.push(
+            h("li", [
+              h("RowWithKey", {
+                props: { rowName: key },
+                on: {
+                  delete() {
+                    deleteData.call(vm.dataTree, createPath(path, key));
+                  },
+                  add() {
+                    // addData.call()
+                  },
+                  toggle() {}
+                }
+              }),
+
+              // invoke function recursively
+              create(value, createPath(path, key))
+            ])
+          );
+        } else {
+          acc.push(
+            h("RowWithKeyAndValue", {
+              props: {
+                rowName: key,
+                rowValue: value
+              },
+              on: {
+                input(value) {
+                  setData.call(vm.dataTree, createPath(path, key), value);
+                }
+              }
+            })
+          );
+        }
+
+        return acc;
+      }, []);
+
+      return h(
+        "ul",
+        {
+          staticClass: "data-tree-list"
+        },
+        list
+      );
     }
   }
+};
 
-  function createPath(path, key) {
-    return path ? `${path}.${key}` : key
+function createPath(path, key) {
+  return path ? `${path}.${key}` : key;
+}
+
+function setData(path, value) {
+  const { link, lastKey } = getLink.call(this, path);
+  Vue.set(link, lastKey, value);
+}
+
+function deleteData(path) {
+  const { link, lastKey } = getLink.call(this, path);
+  Vue.delete(link, lastKey);
+}
+
+function getLink(path) {
+  let link = this;
+  const keys = path.split(".");
+  const keysCount = keys.length;
+  const lastKey = keys[keysCount - 1];
+
+  for (let i = 0; i < keysCount - 1; i++) {
+    link = link[keys[i]];
   }
 
-  function setData(path, value) {
-    let link = this
-    const keys = path.split('.')
-    const keysCount = keys.length
-    const lastKey = keys[keysCount - 1]
-
-    for (let i = 0; i < keysCount - 1; i++) {
-      link = link[keys[i]]
-    }
-
-    Vue.set(link, lastKey, value)
-  }
+  return { link, lastKey };
+}
 </script>
 
 <style lang="scss">
-  .data-tree {
-    &-list {
-      padding-left: 0;
-      margin-bottom: 0;
-      list-style: none;
+.data-tree {
+  &-list {
+    padding-left: 0;
+    margin-bottom: 0;
+    list-style: none;
 
-      & & {
-        padding-left: 30px;
-      }
+    & & {
+      padding-left: 30px;
     }
   }
+}
 </style>
